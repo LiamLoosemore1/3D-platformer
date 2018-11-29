167f502a4f3066be08137ec57a10b38a4f561ea4
SUSSEXDOWNS\474967 <474967@chrome.sussexdowns.ac.uk>
2018-11-29 14:58:39 +0000

    i have made gold pick up

---
+++ Assets/Scripts/Destroyovertime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyovertime : MonoBehaviour {
+
+    public float lifeTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Destroy(gameObject, lifeTime); 
+
+	}
+}
---
+++ Assets/Scripts/GoldPickUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldPickUp : MonoBehaviour {
+
+    public int value;
+
+    public GameObject pickUpEffect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().AddGold(value);
+
+            Instantiate(pickUpEffect, transform.position, transform.rotation);
+
+            Destroy(gameObject);
+        }
+    }
+}
